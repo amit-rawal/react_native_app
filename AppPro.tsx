c84c8d278e415c8e0bcd79480290dba533fd0171
Amit Rawal <mail2amitrawal@gmail.com>
2024-05-14 12:16:00 +0530

    AppPro file added

---
+++ AppPro.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    SafeAreaView,
+    View,
+    Text
+} from 'react-native';
+
+function AppPro(){
+    return(
+        <SafeAreaView>
+        <View>
+            <Text>Hello</Text>
+        </View>
+        </SafeAreaView>
+    )
+}
+
+export default AppPro
