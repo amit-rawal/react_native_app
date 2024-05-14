822d773185524306b912e0de8985d26d185e66a9
Amit Rawal <mail2amitrawal@gmail.com>
2024-05-14 12:47:32 +0530

    Used StyleSheet, useColorScheme

--- AppPro.tsx
+++ AppPro.tsx
@@ -2,17 +2,33 @@ import React from "react";
 import {
     SafeAreaView,
     View,
-    Text
+    Text,
+    StyleSheet,
+    useColorScheme
 } from 'react-native';
 
-function AppPro(){
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark'
     return(
         <SafeAreaView>
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.container}>
+            <Text style={isDarkMode? styles.whiteText : styles.darkText}>Hello </Text>
         </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: '#FFFF00'
+    },
+    whiteText: {
+        color: '#FFFFFF'
+    },
+    darkText: {
+        color: '#000000'
+    }
+})
 
 export default AppPro
